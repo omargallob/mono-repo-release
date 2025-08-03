9de91653ab1a939158fef1c9800db652369fb026
Omar Gallo <omargallob@gmail.com>
2025-08-03 20:34:48 +0200

    rfemove cyclical import

--- pkg/lib2/main.go
+++ pkg/lib2/main.go
@@ -1,11 +1,9 @@
-package main
+package lib2
 
 import (
 	"fmt"
-
-	"github.com/omargallob/mono-repo-release/pkg/lib2"
 )
 
 func main() {
-	fmt.Println(lib2.Farewell())
+	fmt.Println(Farewell())
 }
