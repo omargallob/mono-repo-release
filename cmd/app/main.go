0781ccf2138af4d2eb7382a052658a1c2880b6cf
Omar Gallo <omargallob@gmail.com>
2025-11-16 19:32:48 +0100

    chore: also building cmd app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -7,6 +7,9 @@ import (
 	"github.com/omargallob/mono-repo-release/pkg/lib2"
 )
 
+// version is set at build time via ldflags. Default to "dev" for local builds.
+var version = "dev"
+
 // main is the entry point for the application.
 func main() {
 	fmt.Println(lib1.Greet())
